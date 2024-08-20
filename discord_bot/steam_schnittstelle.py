657553ec94d62f8b339e6f548dc9de4e5a14cc72
sansdoute <80106368+sansdoute@users.noreply.github.com>
2024-08-20 22:41:47 +0200

    Update steam_schnittstelle.py

--- discord_bot/steam_schnittstelle.py
+++ discord_bot/steam_schnittstelle.py
@@ -5,27 +5,6 @@ from database import fetchStartdataDB
 
 api_key = ''
 
-def getSteamData():
-    # Your Steam API key
-    steam_id = '76561198050659307'
-
-    # URL for the GetPlayerSummaries API endpoint
-    url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_id}'
-
-    # Make the request
-    response = requests.get(url)
-
-    if response.status_code == 200:
-
-        # Parse the JSON response
-        player_summary = response.json()
-
-
-        # Output the result
-        print(player_summary['response']['players'][0]['personastate'])
-        return player_summary['response']['players'][0]['personastate']
-    else: 
-        print("Failed to request from Steam")
 
 
 def getUserInfo(steam_id: str, discord_id):
